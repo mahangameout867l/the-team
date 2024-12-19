96ac379a1f06f04230556778b144f652bfac92a1
mahangameout867l <mmovahedijam@gmail.com>
2024-12-19 19:54:31 +0330

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,12 +1,17 @@
 user_file=input("file name:").lower().strip()
 if user_file.endswith(".gif"):
     print("image/gif")
-elif user_file.endswith(".exe"):
-   print("runfile or adminrun/exe aplication app,shorcut")
 elif user_file.endswith(".jpg"):
-    print("image or .jpg file/jpeg")
+    print("image/jpeg")
 elif user_file.endswith("jpeg"):
    print("image/jpeg")
 elif user_file.endswith(".txt"):
    print("text/plain")
-
+elif user_file.endswith(".pdf"):
+   print("application/pdf")
+elif user_file.endswith(".zip"):
+   print("aplication/zip")
+elif user_file.endswith(".png"):
+   print("image/png")
+else:
+    print("application/octet-stream")
