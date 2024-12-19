001baa36a78abd7a5eba2a6337cca3bd67f133fe
mahangameout867l <mmovahedijam@gmail.com>
2024-12-19 19:53:16 +0330

    deep

---
+++ deep.py
@@ -0,0 +1,6 @@
+true_word=["42","forty-two","forty two"]
+user=input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+if user in true_word:
+    print("Yes")
+else:
+    print("No")
